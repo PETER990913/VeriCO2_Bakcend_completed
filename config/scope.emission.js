5cd199b64c4559bbed6a3c550d5d4187a5232a28
PETER990913 <jeremy.t.1132@gmail.com>
2023-09-19 09:43:34 -0700

    'o'

---
+++ config/scope.emission.js
@@ -0,0 +1,10 @@
+const scope = {
+    "Net Zero": [
+        727358.0, 243247.0
+    ],
+
+    "Dust": [
+        12398.0, 342871.0
+    ]
+}
+module.exports = scope;
--- routes/excel.route.js
+++ routes/excel.route.js
@@ -3,6 +3,8 @@ let mongoose = require('mongoose'),
     router = express.Router()
 const multer = require('multer');
 var xlsx = require('node-xlsx');
+const factor = require('../config/factor.constants');
+const scope = require('../config/scope.emission');
 
 
 const storage = multer.diskStorage({
@@ -36,6 +38,18 @@ router.route('/create-student').post((req, res, next) => {
         }
     })
 })
+// get factor list
+router.route('/load-factor').get((req, res) => {
+    // const factor = false;
+    return res.send(factor)
+})
+
+// get scope list 
+router.route('/load-scope').get((req, res) => {
+    // const factor = false;
+    return res.send(scope)
+})
+
 //Uploading CSV
 router.route('/read-file').post((req, res) => {
     upload(req, res, function (err, file) {
@@ -76,6 +90,48 @@ router.route('/read-file').post((req, res) => {
         }
         console.log('------', data__)
         res.send({ data: data__ });
+    })
+})
+
+router.route('/read-fileCSV').post((req, res) => {
+    upload(req, res, function (err, file) {
+        if (err) {
+            return res.status(500).send({
+                message: err.message
+            })
+        }
+        console.log(req.file.path)
+        // var obj = xlsx.parse(req.file.path); // parses a file
+        // console.log("data", JSON.stringify(obj))
+        // let data = obj[0].data;
+        // let f = 0;
+        // let data_ = [], data__ = [];
+        // for (i in data) {
+        //     let record = data[i];
+        //     console.log(record.length)
+        //     if (record.length == 0) {
+        //         f = 0;
+        //         if (data_.length) {
+        //             data__.push(data_)
+        //             data_ = []
+        //         }
+        //     }
+        //     else if (`${record[0]}`.includes('Category')) {
+        //         f = 1;
+        //     }
+        //     if (f) {
+        //         let arr = [];
+        //         for (let j in record) {
+        //             let number = getNumber(`${record[j]}`)
+        //             if (number)
+        //                 arr.push(number);
+        //         }
+        //         if (arr.length > 1)
+        //             data_.push(arr);
+        //     }
+        // }
+        // console.log('------', data__)
+        // res.send({ data: data__ });
     })
 })
 module.exports = router
